c611ab460418736ba5b19718a8def3ed73c999ee
Martin0708E <150486355+Martin0708E@users.noreply.github.com>
2026-06-30 12:42:51 +0000

    Delete dgmp_acquisitions directory

--- dgmp_acquisitions/test_driver.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (extension_loaded('pdo_pgsql')) {
-    echo "✅ Le driver pdo_pgsql est bien installé !";
-} else {
-    echo "❌ Le driver pdo_pgsql est TOUJOURS MANQUANT.";
-    echo "<br>Extensions installées : " . implode(", ", get_loaded_extensions());
-}
-?>
